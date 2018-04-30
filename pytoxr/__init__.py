d68abb3969e1832f4e7da559050da23edb87c3e2
mark <mark.teese@tum.de>
2018-05-01 00:14:41 +0200

    added an import function

--- pytoxr/__init__.py
+++ pytoxr/__init__.py
@@ -1,7 +1,10 @@
 from pytoxr.parse import parse_softmax
 from pytoxr.parse import parse_all_data_files_in_folder
 from pytoxr.collect import collect_data_in_folder
+from pytoxr.scanmut.scanmut import run_scanmut
 import pytoxr.toolkit
 import pytoxr.scanmut
 import pytoxr.fit_to_sine
-from pytoxr.scanmut.scanmut import run_scanmut
+import pytoxr.mathfunctions
+import pytoxr.collect
+import pytoxr.parse
